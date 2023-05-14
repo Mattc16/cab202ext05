d05a1de72775d7c62295645f2061b88d8bde6250
Matthew Chambers <Matthew@AACsolutions.com.au>
2023-05-14 18:56:16 +1000

    MESSAGE

--- src/extension05.c
+++ src/extension05.c
@@ -44,13 +44,13 @@
 volatile uint8_t state = 0;
 ISR(TCA0_OVF_vect) {
     if (state == 0) {
-        TCA0.SINGLE.PERBUF = 1564;
-        TCA0.SINGLE.CMP0BUF = 3127;
+        TCA0.SINGLE.PERBUF = 9390;
+        TCA0.SINGLE.CMP0BUF = 18779;
         _delay_ms(350);
         state = 1;
     } else {
-        TCA0.SINGLE.PERBUF = 797;
-        TCA0.SINGLE.CMP0BUF = 1594;
+        TCA0.SINGLE.PERBUF = 7485;
+        TCA0.SINGLE.CMP0BUF = 9569;
         _delay_ms(640);
         state = 0;
     }
